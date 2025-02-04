a12bd3571e08fed33946e8adcf71492d05671c96
Regyung <pho5000@naver.com>
2025-02-04 22:18:21 +0900

    feat(calculator): add custom operator
    앞의 기본 구분자(쉼표, 콜론) 외에 커스텀 구분자를 지정할 있다.커스텀 구분자는 문자열 앞부분의 "//"와 "\n" 사이에 위치하는 문자를 커스텀 구분자로 사용한다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -3,18 +3,29 @@ package calculator;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
         // 값 입력받기
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String word = Console.readLine();
-        System.out.println(word);
 
-        // 구분자 정리
+        // 기본 구분자 설정
         ArrayList<String> operatorList = new ArrayList<String>();
         operatorList.add(",");
         operatorList.add(":");
+
+        // 커스텀 구분자 인식
+        Pattern p = Pattern.compile("//\\D\\\\n");
+        Matcher m = p.matcher(word);
+        if (m.find()) {
+            operatorList.add(String.valueOf(word.charAt(m.start()+2)));
+        }
+        word = word.substring(m.end());
+
+        // 구분자 정리
         String[] operatorArray = new String[operatorList.size()];
         for (int i = 0; i < operatorList.size(); i++)
             operatorArray[i] = operatorList.get(i);
